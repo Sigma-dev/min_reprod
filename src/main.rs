484ebf880a138536712cbb917e440380acf72d5e
Sigma-dev <antonin.programming@gmail.com>
2024-09-08 12:41:12 +0200

    Added joining

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_steamworks::{Client, LobbyId, LobbyType, SteamworksPlugin};
+use bevy_steamworks::{Client, LobbyId, LobbyType, SteamworksEvent, SteamworksPlugin};
 use flume::{Receiver, Sender};
 use steamworks::networking_types::{NetworkingIdentity, SendFlags};
 
@@ -79,15 +79,37 @@ fn update(
 fn receive(
     mut client: ResMut<NetworkClient>,
     steam_client: Res<Client>,
+    mut evs: EventReader<SteamworksEvent>,
 ) {
     let rx: Receiver<LobbyId> = client.rx.clone();
     if let Ok(lobby_id) = rx.try_recv() {
         client.lobby_id = Some(lobby_id);
         println!("Joined Lobby: {}", lobby_id.raw());
     }
+
     let messages: Vec<steamworks::networking_types::NetworkingMessage<steamworks::ClientManager>> = steam_client.networking_messages().receive_messages_on_channel(0, 1);
     for message in messages {
         println!("Received message");
         drop(message); //not sure about usefullness, mentionned in steam docs as release
+    }
+
+    for ev in evs.read() {
+        //println!("EV");
+        match ev {
+            SteamworksEvent::GameLobbyJoinRequested(info) => {
+                println!("Trying to join: {}", info.lobby_steam_id.raw());
+                let tx = client.tx.clone();
+                steam_client.matchmaking().join_lobby(info.lobby_steam_id, 
+                    move |res| {
+                        if let Ok(lobby_id) = res {
+                            match tx.send(lobby_id) {
+                                Ok(_) => {}
+                                Err(_) => {
+                                }
+                            }
+                        }
+                    });            },
+            _ => {}
+        }
     }
 }
